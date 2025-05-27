9bdcbb4bd7f26803d0394cd3ff82765e557a7e38
Katherina Dayaon <katherinadayaon@gmail.com>
2025-05-27 16:24:39 -0400

    added some features to the about page

--- portfolio/components/BottomBar.tsx
+++ portfolio/components/BottomBar.tsx
@@ -5,7 +5,7 @@ export default function BottomBar() {
     };
 
     return (
-        <div className = "w-full flex bg-kat-black p-4 max-h-20 min-w-100 justify-center  space-x-20 items-center align-middle text-lg md:text-3xl">
+        <div className = "w-full flex bg-kat-black p-4 max-h-20 min-w-screen justify-center  space-x-20 items-center align-middle text-lg md:text-3xl">
                 <button onClick={() => openNewTab('/assets/KatherinaDayaon_Resume.pdf')}>
                         <img className="hover:brightness-200 cursor-pointer" src="/assets/resume.svg"></img>
                     </button>
